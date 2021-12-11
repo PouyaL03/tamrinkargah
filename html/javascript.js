0fb3f0784425bbb71ba9ba28429f71f9465755fc
PouyaL <pouyal@DESKTOP-NVEM0BD.localdomain>
2021-12-11 11:53:54 +0330

    tamrine kargah done

---
+++ html/javascript.js
@@ -0,0 +1,10 @@
+function func(){
+    document.getElementById("text");
+}
+
+function submit(){
+    var temp=confirm("آیا از ارسال فرم خود اطمینان دارید؟");
+    if (temp==true){
+        alert("درخواست شما با موفقیت انجام شد.");
+    }
+}
